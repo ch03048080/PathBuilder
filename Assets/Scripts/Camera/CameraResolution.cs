b3e8df944d4a702c2cfe6fd83e2fc566db2e4d69
전혜진 <akfltmxpffk0@naver.com>
2025-08-29 18:38:17 +0900

    organize file

--- Assets/Scripts/Camera/CameraResolution.cs
+++ Assets/Scripts/Camera/CameraResolution.cs
@@ -2,34 +2,31 @@ using UnityEngine;
 // 카메라 관리
 public class CameraResolution : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   private void Awake()
+    [SerializeField] private float targetAspectWidth = 16f;
+    [SerializeField] private float targetAspectHeight = 9f;
+
+    private void Awake()
     {
-   
         Camera cam = GetComponent<Camera>();
-
-  
         Rect viewportRect = cam.rect;
+        //Debug.Log($"Screen.width: {Screen.width}, Screen.height: {Screen.height}");
+        //Debug.Log($"Camera.pixelWidth: {cam.pixelWidth}, Camera.pixelHeight: {cam.pixelHeight}");
+        //Debug.Log($"Camera.rect: {cam.rect}");
 
-        // 원하는 가로 세로 비율을 계산
         float screenAspectRatio = (float)Screen.width / Screen.height;
-        float targetAspectRatio = 16f / 9f;
+        float targetAspectRatio = targetAspectWidth / targetAspectHeight;
 
-        // 화면 가로 세로 비율에 따라 뷰포트 영역 조정
         if (screenAspectRatio < targetAspectRatio)
         {
-            //세로가 더 길다면, 세로를 조절
             viewportRect.height = screenAspectRatio / targetAspectRatio;
             viewportRect.y = (1f - viewportRect.height) / 2f;
         }
         else
         {
-            //가로가 더 길다면, 가로를 조절
             viewportRect.width = targetAspectRatio / screenAspectRatio;
             viewportRect.x = (1f - viewportRect.width) / 2f;
         }
 
-        // 조정된 뷰포트 영역을 카메라에 설정
         cam.rect = viewportRect;
     }
 }
